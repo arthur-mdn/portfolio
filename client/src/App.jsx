426d28300e11f307a866581079deae1b88ba59a8
Arthur <arthur@mondon.pro>
2023-12-19 13:09:10 +0100

    manage routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,26 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
 import Menu from "./components/menu.jsx";
-import HeroSection from "./components/HeroSection.jsx";
-import AboutSection from "./components/AboutSection.jsx";
 import SkillsSection from "./components/SkillsSection.jsx";
 import ProjectsSection from "./components/ProjectsSection.jsx";
-import LinksSection from "./components/LinksSection.jsx";
+import HomePage from "./components/HomePage.jsx";
+import ContactPage from "./components/ContactPage.jsx"; // Supposons que vous ayez un composant pour la section Contact
 
 function App() {
-  return (
-    <>
-      <Menu/>
-      <div style={{height:'80px'}}>
-      </div>
-      <HeroSection/>
-      <br/>
-      <br/>
-      <AboutSection/>
-      <br/>
-      <SkillsSection/>
-      <ProjectsSection/>
-        <LinksSection/>
-        <br/>
-        <br/>
-    </>
-  )
+    return (
+        <Router>
+            <Menu />
+            <Routes>
+                <Route path="/" element={
+                   <HomePage />
+                } />
+                <Route path="/competences" element={<SkillsSection />} />
+                <Route path="/projets" element={<ProjectsSection />} />
+                <Route path="/contact" element={<ContactPage />} />
+            </Routes>
+        </Router>
+    );
 }
 
-export default App
+export default App;
---
+++ client/src/components/HomePage.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import projectsData from "/data/projects.json";
+import {FaGithub, FaLinkedin} from "react-icons/fa6";
+import HeroSection from "./HeroSection.jsx";
+import AboutSection from "./AboutSection.jsx";
+import SkillsSection from "./SkillsSection.jsx";
+import ProjectsSection from "./ProjectsSection.jsx";
+import LinksSection from "./LinksSection.jsx";
+
+function HomePage() {
+
+    return (
+        <>
+            <>
+                <HeroSection />
+                <AboutSection />
+                <SkillsSection />
+                <ProjectsSection />
+                <LinksSection />
+            </>
+        </>
+    );
+}
+
+export default HomePage;
