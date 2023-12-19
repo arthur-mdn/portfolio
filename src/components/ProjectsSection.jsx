ed20bbbb240258bfa8195c27e0e66051b03bfec6
Arthur <arthur@mondon.pro>
2023-12-19 21:09:59 +0100

    debug 02

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import projectsData from "../data/projects.json";
 import {FaExternalLinkAlt} from "react-icons/fa";
 import {FaArrowRightLong, FaEye, FaListCheck} from "react-icons/fa6";
-import Carousel from 'better-react-carousel'
 import {Link} from "react-router-dom";
 
 function ProjectsSection() {
@@ -13,32 +12,10 @@ function ProjectsSection() {
         <>
             <section className="PS fc g1">
                 <h2>Projets</h2>
-                <Carousel showDots scrollSnap gap={10} rows={1} autoplay={3000} loop responsiveLayout={[
-                    {
-                        breakpoint: 2000,
-                        cols: 5
-                    },
-                    {
-                        breakpoint: 1000,
-                        cols: 4
-                    },
-                    {
-                        breakpoint: 800,
-                        cols: 3
-                    },
-                    {
-                        breakpoint: 500,
-                        cols: 4
-                    },
-                    {
-                        breakpoint: 150,
-                        cols: 4
-                    }
 
-                ]} >
                     {sortedProjects.map(project => (
                         // on click, redirect to page /projects/:id
-                        <Carousel.Item key={project.id} >
+                        <div key={project.id} >
                             <Link to={`/projects/${project.slug}`} className="PS_card">
                                 <img src={`/${project.image}`} alt={project.name} />
                                 <div className={"content"}>
@@ -48,9 +25,8 @@ function ProjectsSection() {
                                     <button type={"button"} className={"button"}><FaArrowRightLong/></button>
                                 </div>
                             </Link>
-                        </Carousel.Item>
+                        </div>
                     ))}
-                </Carousel>
                 <Link to="/projets" className={"button c-white"} style={{width:"fit-content",margin:'0.8rem auto'}}>
                     <FaEye/>Voir tous les projets
                 </Link>
