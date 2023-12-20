400c03c22b7c2cb98b9eed2bae2fe1623bfe542d
Arthur <arthur@mondon.pro>
2023-12-20 11:23:08 +0100

    upgrade herosection responsivness

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import SkillsSection from "./components/SkillsSection.jsx";
 import HomePage from "./components/HomePage.jsx";
 import ContactPage from "./components/ContactPage.jsx";
 import ProjectsPage from "./components/ProjectsPage.jsx";
-import ProjectPage from "./components/ProjectPage.jsx"; // Supposons que vous ayez un composant pour la section Contact
+import ProjectPage from "./components/ProjectPage.jsx";
+import NotFound from "./components/NotFound.jsx"; // Supposons que vous ayez un composant pour la section Contact
 
 function App() {
     return (
@@ -17,6 +18,7 @@ function App() {
                 <Route path="/projets" element={<ProjectsPage />} />
                 <Route path="/projects/:slug" element={<ProjectPage />} />
                 <Route path="/contact" element={<ContactPage />} />
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </Router>
     );
