566d81bffda6710252957ba5c07a84798972f2a3
Arthur <arthur@mondon.pro>
2023-12-20 01:09:45 +0100

    carousel on homescreen

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,36 +1,89 @@
 import React from "react";
 import projectsData from "../data/projects.json";
-import {FaExternalLinkAlt} from "react-icons/fa";
-import {FaArrowRightLong, FaEye, FaListCheck} from "react-icons/fa6";
+import {FaArrowLeft,FaArrowRight, FaArrowRightLong, FaEye} from "react-icons/fa6";
 import {Link} from "react-router-dom";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 function ProjectsSection() {
-    // Trier les projets par date dans l'ordre décroissant
-    const sortedProjects = projectsData.sort((a, b) => new Date(b.date) - new Date(a.date));
+    const settings = {
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        nextArrow: <SampleNextArrow />,
+        prevArrow: <SamplePrevArrow />,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    };
+
+// Composants pour les boutons personnalisés
+    function SamplePrevArrow(props) {
+        const { className, style, onClick } = props;
+        return (
+            <div
+                className={className}
+                style={{ ...style }}
+                onClick={onClick}
+            >
+                <FaArrowLeft/>
+            </div>
+        );
+    }
+
+    function SampleNextArrow(props) {
+        const { className, style, onClick } = props;
+        return (
+            <div
+                className={className}
+                style={{ ...style }}
+                onClick={onClick}
+            >
+                <FaArrowRight/>
+            </div>
+        );
+    }
+
 
     return (
         <>
             <section className="PS fc g1">
                 <h2>Projets</h2>
-
-                    {sortedProjects.map(project => (
-                        // on click, redirect to page /projects/:id
-                        <div key={project.id} >
-                            <Link to={`/projects/${project.slug}`} className="PS_card">
+                <Slider {...settings}>
+                    {projectsData.map(project => (
+                        <div key={project.id} className="PS_card">
+                            <Link to={`/projects/${project.slug}`} style={{width:'100%'}}>
                                 <img src={`/${project.image}`} alt={project.name} />
-                                <div className={"content"}>
+                                <div className="content">
                                     <p>{project.type}</p>
                                     <span>{project.date}</span>
                                     <h3>{project.name}</h3>
-                                    <button type={"button"} className={"button"}><FaArrowRightLong/></button>
+                                    <button type="button" className="button"><FaArrowRightLong/></button>
                                 </div>
                             </Link>
                         </div>
                     ))}
-                <Link to="/projets" className={"button c-white"} style={{width:"fit-content",margin:'0.8rem auto'}}>
-                    <FaEye/>Voir tous les projets
+                </Slider>
+                <Link to="/projets" className="button c-white" style={{width: "fit-content", margin: '1.5rem auto'}}>
+                    <FaEye/> Voir tous les projets
                 </Link>
-
             </section>
         </>
     );
