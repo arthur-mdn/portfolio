90506b60b51919e0896275d66fbd9da78c62cfff
Arthur <arthur@mondon.pro>
2024-01-11 10:49:14 +0100

    make cliquable skills icons

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -1,9 +1,40 @@
 import { useState } from "react";
-import skillsData from "../data/skills.json"; // Chemin relatif à votre composant
+import skillsData from "../data/skills.json";
+import projects from "../data/projects.json";
 
+function SkillElement({ skill, displayedPopup, setDisplayedPopup }) {
+    const isPopupDisplayed = displayedPopup === skill.id;
+
+    const togglePopup = () => {
+        if (isPopupDisplayed) {
+            setDisplayedPopup(null); // Ferme la popup si elle est déjà ouverte
+        } else {
+            setDisplayedPopup(skill.id); // Ouvre la popup pour cette compétence
+        }
+    };
+
+    const projectCount = projects.filter(project =>
+        project.technos.includes(skill.id)
+    ).length;
+
+    return (
+        <button title={skill.description} onClick={togglePopup}>
+            {isPopupDisplayed &&
+                <div className={"popup"}>
+                    <p>Projets publics utilisant {skill.description} : {projectCount}</p>
+                </div>
+            }
+
+            <img src={"/"+skill.links[0].url} alt={skill.links[0].alt} />
+            <p className={"s-font fs0-8"}>{skill.name}</p>
+            <p style={{fontSize:'0.8rem',opacity:'0.6'}}>{skill.id}</p>
+        </button>
+    );
+}
 function SkillsSection() {
     // État pour suivre la catégorie actuelle
     const [activeCategory, setActiveCategory] = useState('All');
+    const [displayedPopup, setDisplayedPopup] = useState(null); // Nouvel état pour la popup affichée
 
     // Filtrer les compétences en fonction de la catégorie active
     const filteredSkills = activeCategory === 'All'
@@ -34,11 +65,12 @@ function SkillsSection() {
                 <div className="skills-icons g0-5">
                     {filteredSkills.map(category =>
                         category.skills.map(skill => (
-                            <div key={skill.name} title={skill.description}>
-                                <img src={"/"+skill.links[0].url} alt={skill.links[0].alt} />
-                                <p className={"s-font fs0-8"}>{skill.name}</p>
-                                <p style={{fontSize:'0.8rem',opacity:'0.6'}}>{skill.id}</p>
-                            </div>
+                            <SkillElement
+                                key={skill.id}
+                                skill={skill}
+                                displayedPopup={displayedPopup}
+                                setDisplayedPopup={setDisplayedPopup}
+                            />
                         ))
                     )}
                 </div>
