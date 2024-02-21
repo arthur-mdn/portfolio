9a0d48b6f01f64d1471b2c4aeff74f7aeda57520
Arthur <arthur@mondon.pro>
2024-02-21 12:57:29 +0100

    shorten meta description

--- pages/index.jsx
+++ pages/index.jsx
@@ -55,7 +55,7 @@ export default function Competences() {
                 <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaOrgJSONLD) }}></script>
                 <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaOrgJSONLDPerson) }}></script>
                 <meta name="description"
-                      content="Je suis Arthur Mondon, développeur web passionné. À travers mon portfolio, découvrez ma passion pour la création de solutions web innovantes. En tant qu'auto-entrepreneur et freelance, j'ai créé 'Eradion' pour réaliser des projets numériques captivants. Ma spécialité réside dans le mélange de compétences techniques et de créativité, visant à offrir des expériences en ligne inoubliables. Actuellement étudiant en BUT MMI à Toulon. Je propose mes services en Provence-Alpes-Côte-d'Azur dans le Vaucluse et le Var."/>
+                      content="Arthur Mondon, développeur web fullstack freelance. Solutions web innovantes. Découvrez mon portfolio répertoriant mes projets publics. Originaire du Vaucluse."/>
                 <meta name="keywords"
                       content="Arthur Mondon, développeur web, portfolio, solutions web, innovation numérique, auto-entrepreneur, freelance, Eradion, projets numériques, création web, compétences techniques, créativité, expérience en ligne, BUT MMI, Toulon, services informatiques, Provence-Alpes-Côte-d'Azur, Vaucluse, Var"/>
                 <meta name="author" content="Arthur MONDON"/>
@@ -65,7 +65,7 @@ export default function Competences() {
                 <meta property="og:type" content="website"/>
                 <meta property="og:title" content="Arthur Mondon : Portfolio"/>
                 <meta property="og:description"
-                      content="Je suis Arthur Mondon, développeur web passionné. À travers mon portfolio, découvrez ma passion pour la création de solutions web innovantes. En tant qu'auto-entrepreneur et freelance, j'ai créé 'Eradion' pour réaliser des projets numériques captivants. Ma spécialité réside dans le mélange de compétences techniques et de créativité, visant à offrir des expériences en ligne inoubliables. Actuellement étudiant en BUT MMI à Toulon. Je propose mes services en Provence-Alpes-Côte-d'Azur dans le Vaucluse et le Var."/>
+                      content="Arthur Mondon, développeur web fullstack freelance. Solutions web innovantes. Découvrez mon portfolio répertoriant mes projets publics. Originaire du Vaucluse."/>
                 <meta property="og:image" content="https://mondon.pro/others/preview.png"/>
 
                 <meta name="twitter:card" content="summary_large_image"/>
@@ -73,7 +73,7 @@ export default function Competences() {
                 <meta property="twitter:url" content="https://mondon.pro"/>
                 <meta name="twitter:title" content="Arthur Mondon : Portfolio"/>
                 <meta name="twitter:description"
-                      content="Je suis Arthur Mondon, développeur web passionné. À travers mon portfolio, découvrez ma passion pour la création de solutions web innovantes. En tant qu'auto-entrepreneur et freelance, j'ai créé 'Eradion' pour réaliser des projets numériques captivants. Ma spécialité réside dans le mélange de compétences techniques et de créativité, visant à offrir des expériences en ligne inoubliables. Actuellement étudiant en BUT MMI à Toulon. Je propose mes services en Provence-Alpes-Côte-d'Azur dans le Vaucluse et le Var."/>
+                      content="Arthur Mondon, développeur web fullstack freelance. Solutions web innovantes. Découvrez mon portfolio répertoriant mes projets publics. Originaire du Vaucluse."/>
                 <meta name="twitter:image" content="https://mondon.pro/others/preview.png"/>
             </Head>
             <HeroSection/>
