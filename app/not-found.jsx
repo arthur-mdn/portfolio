3d49547181b49c2ae390e006b818f30ec8145f4d
Arthur Mondon <arthur@mondon.pro>
2024-05-27 21:56:08 +0200

    fix 404 page

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,20 +1,33 @@
-// pages/not-found.js
+"use client";
+
+// pages/not-found.jsx
+import '../styles/globals.css';
 import Image from "next/image";
-export default function Custom404() {
+import Menu from "@/components/Menu";
+import Footer from "@/components/Footer";
+export default function NotFound() {
     const goBack = () => {
         window.history.back();
     };
     return (
+        <html lang="fr">
+        <body>
+        <Menu/>
         <section>
             <div className={"fc f-c ai-c g2"}>
                 <h1 style={{textAlign: "center"}}>
                     404 - Page introuvable
                 </h1>
-                <Image src={"/others/404.svg"} alt={"404"} width={300} height={300} style={{width:"100%"}}/>
+                <Image src={"/others/404.svg"} alt={"404"} width={300} height={300} style={{width: "100%"}}/>
                 <button type={"button"} onClick={goBack} className={"button skew"}>Retour</button>
             </div>
             <br/>
             <br/>
         </section>
+        <Footer/>
+
+        </body>
+        </html>
+
     );
 }
