5625e0c2061e50c3ee46e503e5f0f76529e55b53
Arthur Mondon <arthur@mondon.pro>
2024-05-29 00:59:40 +0200

    prepare first article

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -3,7 +3,10 @@ import Head from "next/head";
 import Link from "next/link";
 import articlesData from "../../data/articles.json";
 import ReactMarkdown from "react-markdown";
-import { FaBan, FaGithub, FaLink, FaArrowUp, FaArrowDown, FaExclamation, FaTriangleExclamation, FaUser } from "react-icons/fa";
+import rehypeRaw from "rehype-raw";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { okaidia } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import {FaRegCalendar} from "react-icons/fa6";
 
 export async function getStaticPaths() {
     const paths = articlesData.map(article => ({
@@ -25,12 +28,26 @@ export async function getStaticProps({ params }) {
 function ArticlePage({ article }) {
     const [likes, setLikes] = useState(article.likes);
     const [comments, setComments] = useState(article.comments);
+    const [isImageExpanded, setIsImageExpanded] = useState(false);
     const [newComment, setNewComment] = useState("");
+
+    useEffect(() => {
+        if (article.js_to_inject) {
+            const script = document.createElement("script");
+            script.innerHTML = article.js_to_inject;
+            document.body.appendChild(script);
+            return () => {
+                document.body.removeChild(script);
+            };
+        }
+    }, [article.js_to_inject]);
 
     const handleLike = () => {
         setLikes(likes + 1);
     };
-
+    const handleImageClick = () => {
+        setIsImageExpanded(!isImageExpanded);
+    };
     const handleAddComment = () => {
         if (newComment.trim()) {
             setComments([
@@ -52,6 +69,21 @@ function ArticlePage({ article }) {
         return `${day}/${month}/${year}`;
     };
 
+    const renderers = {
+        code({node, inline, className, children, ...props}) {
+            const match = /language-(\w+)/.exec(className || '')
+            return !inline && match ? (
+                <SyntaxHighlighter style={okaidia} language={match[1]} PreTag="div" {...props}>
+                    {String(children).replace(/\n$/, '')}
+                </SyntaxHighlighter>
+            ) : (
+                <code className={className} {...props}>
+                    {children}
+                </code>
+            )
+        }
+    };
+
     return (
         <>
             <Head>
@@ -62,27 +94,35 @@ function ArticlePage({ article }) {
                 <meta property="og:image" content={`https://yourwebsite.com/${article.cover_image}`} />
                 <meta property="og:type" content="article" />
             </Head>
-            <article>
-                <header>
-                    <h1>{article.title}</h1>
-                    <div className="article-meta">
-                        <img src={`/${article.author.profile_image}`} alt={article.author.name} className="author-image" />
-                        <div>
-                            <h4>{article.author.name}</h4>
-                            <p>{article.author.bio}</p>
+            <div className={"blog-bg"}>
+            </div>
+            <article className={"fc g1"}>
+                <header className={"fc g1"}>
+                    <div className={"fc g0-5 fw-w jc-sb"} style={{maxWidth:"900px"}}>
+                        <div className="article-meta BP_card">
+                            <div className={"fr g0-5 ai-c"}>
+                                <img src={`/${article.author.profile_image}`} alt={article.author.name}
+                                     className="author-image"/>
+                                <div className={"fc"}>
+                                    <h4>{article.author.name}</h4>
+                                    <p>{article.author.bio}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={"fr ai-c g0-5 o0-25"}>
+                            <FaRegCalendar/>
                             <time dateTime={article.date}>{formatDate(article.date)}</time>
                         </div>
                     </div>
-                    <img src={`/${article.cover_image}`} alt={article.title} className="cover-image" />
                 </header>
-                <ReactMarkdown>{article.content}</ReactMarkdown>
+                <ReactMarkdown rehypePlugins={[rehypeRaw]} components={renderers}>{article.content.replace(/\[BASE_URL\]/g, "http://localhost:3000/")}</ReactMarkdown>
                 <div className="article-interactions">
                     <button onClick={handleLike}>Like ({likes})</button>
                     <div className="comments">
                         <h3>Comments</h3>
                         {comments.map(comment => (
                             <div key={comment.id} className="comment">
-                                <img src={`/${comment.author_profile_image}`} alt={comment.author} />
+                                <img src={`/${comment.author_profile_image}`} alt={comment.author}/>
                                 <div>
                                     <h4>{comment.author}</h4>
                                     <p>{comment.content}</p>
@@ -107,8 +147,8 @@ function ArticlePage({ article }) {
                             const relatedArticle = articlesData.find(a => a.id === id);
                             return (
                                 <Link key={relatedArticle.id} href={`/blog/${relatedArticle.slug}`}>
-                                        <img src={`/${relatedArticle.cover_image}`} alt={relatedArticle.title} />
-                                        <h4>{relatedArticle.title}</h4>
+                                    <img src={`/${relatedArticle.cover_image}`} alt={relatedArticle.title} />
+                                    <h4>{relatedArticle.title}</h4>
                                 </Link>
                             );
                         })}
